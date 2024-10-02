3c191327e5d3c866c7f8bfa517292d0de946d8c1
Kishore Kanala <kishorekanala@gmail.com>
2024-10-02 15:30:12 +0530

    Refactor main.go file structure and add initial comments

--- main.go
+++ main.go
@@ -1,3 +1,31 @@
+/*
+main.go
+
+This file is part of the nictestcasegen project.
+It contains the main entry point for the application.
+
+The main package prints "Hello, World!" to the console.
+Additional functionality can be added where indicated.
+
+Usage:
+	go run main.go
+
+Author: Kishore
+Created: YYYY-MM-DD
+*/
+// main.go
+//
+// This file is part of the nictestcasegen project.
+// It contains the main entry point for the application.
+//
+// The main package prints "Hello, World!" to the console.
+// Additional functionality can be added where indicated.
+//
+// Usage:
+//   go run main.go
+//
+// Author: Kishore
+// Created: YYYY-MM-DD
 package main
 
 import "fmt"
